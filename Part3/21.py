97177b36c0988d0869ab863d9ff8bbd9fbf376a0
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-06-26 01:17:29 +0900

    Solve 21

--- Part3/21.py
+++ Part3/21.py
@@ -6,10 +6,13 @@ file = open('jawiki-country.json')
 lines = file.readlines()
 file.close()
 
-out = open('britain.json', 'w')
+bri = ''
 for line in lines:
     data = json.loads(line)
+
     if data.get('title') == u'イギリス':
-        json.dump(data.get('text'), out, sort_keys = True, indent = 4)
-    bri = data.get('text') if data.get('title') == u'イギリス' else ""
-    print bri,
+        bri = data.get('text')
+
+for line in bri.split("\n"):
+    if 'Category' in line:
+        print line
