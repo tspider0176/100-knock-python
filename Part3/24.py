55520b72b2b57dd75289f738929b9d23edeb269a
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-07-06 15:32:53 +0900

    Solve 24

---
+++ Part3/24.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+import json, re
+
+file = open('jawiki-country.json')
+lines = file.readlines()
+file.close()
+
+bri = ''
+for line in lines:
+    data = json.loads(line)
+
+    if data.get('title') == u'イギリス':
+        bri = data.get('text')
+
+for line in bri.split("\n"):
+    if u"ファイル" in line:
+        print re.search(r":(.*)\.(\w*)", line).group(0)
